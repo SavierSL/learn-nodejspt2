eb36237225ddd585828aaea3d29c36515f81fe96
Xavier San Lorenzo <sanlorenzoxavier@gmail.com>
2020-12-03 17:02:50 +0800

    init

--- notes-app/app.js
+++ notes-app/app.js
@@ -17,3 +17,100 @@ console.log(printNotes);
 console.log(sum);
 
 //IMPORTING NPM MODULES
+
+// NOT ES6
+const validator = require("validator"); // Email Validator
+console.log(validator.isEmail("sanlorenzo@gmail.com")); // true
+console.log(validator.isEmail("sanlorenzo.com")); // false
+console.log(validator.isURL("https://fb.com")); // Url validator
+
+// PRINTING COLOR
+const chalk = require("chalk");
+const success = chalk.bgGreen.white.bold;
+const error = chalk.bgRed.white.bold;
+
+console.log(success("success"));
+console.log(error("failed"));
+
+// GLOBAL NPM MODULES AND NODEMON
+// npm i nodemon -g, addded - g to install in your operating system
+console.log(success("working"));
+
+// GETTING INPUT FROM USER
+
+// console.log(
+//   process.argv[2]
+// );
+/* node app.js Xavier 
+will output this if no [2] if it has [2] it will show Xavier only
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\User\\Desktop\\learn-node\\notes-app\\app.js',
+  'Xavier'
+]
+*/
+
+// const command = process.argv[2];
+// const commandTwo = process.argv[3]; // node app.js Xavier Natsu
+// if (command === "add") {
+//   console.log("Add your note");
+// } else if (command === "remove") {
+//   console.log("You can remove an item");
+// } else {
+//   console.log("No command found");
+// }
+// console.log(commandTwo); // Natsu
+
+// ARGUMENT PARSING WITH YARGS: part 1
+const yargs = require("yargs"); // node app.js add --title='Xavier'
+// console.log(yargs.argv); // { _: [ 'add' ], title: 'Xavier', '$0': 'app.js' }
+// const one = yargs.argv._;
+// const two = yargs.argv.title;
+// const three = yargs.argv.$0;
+// console.log(one); //["add"];
+// console.log(two); //Xavier
+// console.log(three); //app.js
+
+// customize yargs version
+yargs.version("1.1.0");
+
+//add, remove, read, list
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note");
+  },
+});
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing a note");
+  },
+});
+
+//create a list command
+yargs.command({
+  command: "list",
+  describe: "List",
+  handler: function () {
+    console.log("Listing");
+  },
+});
+
+//create a read command
+yargs.command({
+  command: "read",
+  describe: "reading",
+  handler: function () {
+    console.log("Read now");
+  },
+});
+console.log(yargs.argv);
+
+//Argument Parsing with Yargs part 2
